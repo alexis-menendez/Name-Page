74cd2504dd81efcb71332f75885331f3a7d36fbf
Alexis Menendez <alexis.menendez@austincc.edu>
2025-03-11 19:05:28 -0500

    removed to do
    was only temp storing them in this repo anyways

--- TODO 10/db/seeds.sql
+++
@@ -1,7 +0,0 @@
--- File path: rolecall/db/seeds.sql
-
---  TODO: Insert departments into the department table
-
---  TODO: Insert job roles into the role table, linking each role to a department
-
---  TODO: Insert employees into the employee table, linking each employee to a role and optionally to a manager
